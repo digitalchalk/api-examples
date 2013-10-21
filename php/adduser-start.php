c3ed8389325ee598640d6e52b0092e7071657ffd
Bob Robinson <bobrob@sprucepine.com>
2013-10-21 10:51:50 -0400

    First php directory commit

---
+++ php/adduser-start.php
@@ -0,0 +1,11 @@
+<?php
+
+	require_once('apiconfig.php');
+
+?>
+<html>
+<head><title>Add User Example</title></head>
+<body>
+
+</body>
+</html>
---
+++ php/apiconfig.php
@@ -0,0 +1,7 @@
+<?php
+	
+	$oauthToken = '2f9c7bcb-9fbe-4726-a4f2-ebcf16604de0';
+	$orgHostname = 'myhost.localhost.com';
+	
+?>
+	
